975c1f3af327f6d1f3c499adfff7aff50bdc4972
Jefferson Brito <jefferson.brit@gmail.com>
2020-08-03 15:49:36 -0300

    Fallback

--- creditcard-microservice/src/main/java/br/com/itau/creditcard/client/CustomerClientFallback.java
+++ creditcard-microservice/src/main/java/br/com/itau/creditcard/client/CustomerClientFallback.java
@@ -1,5 +1,6 @@
 package br.com.itau.creditcard.client;
 
+import br.com.itau.creditcard.exception.CustomerServiceOfflineException;
 import br.com.itau.creditcard.model.dto.Customer;
 
 import java.util.Optional;
@@ -7,6 +8,6 @@ import java.util.Optional;
 public class CustomerClientFallback implements CustomerClient {
     @Override
     public Optional<Customer> findCustomerById(Long id) {
-        return null;
+        throw new CustomerServiceOfflineException();
     }
 }
